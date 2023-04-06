3445be93eb7e2fdb44c723b1c1dba2306522d649
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-06 07:56:19 +0530

    minor updates

--- common/components/Footer/index.tsx
+++ common/components/Footer/index.tsx
@@ -13,7 +13,10 @@ const Footer = () => {
         </LinkWithUnderline>
       </span>
 
-      <a href={"#intro"} className="flex items-center gap-2">
+      <a
+        href={"#intro"}
+        className="relative flex items-center gap-2 outline-none after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[5%] after:origin-left after:scale-x-0 after:bg-current after:transition-transform after:duration-200 hover:after:scale-x-100 focus:after:scale-x-100 dark:after:bg-current"
+      >
         Back to top
         <AiOutlineArrowUp />
       </a>
